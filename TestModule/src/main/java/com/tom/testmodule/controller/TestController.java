73f663f7fecd5de017fca30ec7b0601fa7f76562
Tom <1755619015@qq.com>
2023-12-05 13:00:03 +0800

    update testModule

--- TestModule/src/main/java/com/tom/testmodule/controller/TestController.java
+++ TestModule/src/main/java/com/tom/testmodule/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.tom.testmodule.controller;
 
-import com.alibaba.druid.stat.DruidStatManagerFacade;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,13 +14,6 @@ import java.util.Map;
 @RestController
 public class TestController {
 
-    /**获取 druid 数据监控信息
-     * http://127.0.0.1:8080/myDruid/stat
-     */
-    @GetMapping("myDruid/stat")
-    public List<Map<String, Object>> druidStat() {
-        List<Map<String, Object>> statDataList = DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
-        return statDataList;
-    }
+
 
 }
