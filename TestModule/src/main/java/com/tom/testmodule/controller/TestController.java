4121ccae1c25788f1ad842908fd00dffcb580df1
Tom <1755619015@qq.com>
2023-12-27 10:46:32 +0800

    update testModule

--- TestModule/src/main/java/com/tom/testmodule/controller/TestController.java
+++ TestModule/src/main/java/com/tom/testmodule/controller/TestController.java
@@ -1,15 +1,18 @@
 package com.tom.testmodule.controller;
 
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author: Tom
- * @date: 2023/11/27 10:05
+ * @date:
  * @description:
  */
 @RestController
 public class TestController {
-
 
 }
 
